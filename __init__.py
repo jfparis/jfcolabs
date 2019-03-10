25db3bcc5e87fdb281574ec789f5bbef38c889c2
Jean-François Paris <jfparis@rouge.eu.org>
2019-03-10 15:26:23 +0000

    Update

--- __init__.py
+++ __init__.py
@@ -2,11 +2,16 @@ from google.colab import drive
 import os.path
 
 
-DEFAULT_PATH='/content/gdrive'
+DEFAULT_PATH = '/content/gdrive'
+
+
+def get_drive_root(path=DEFAULT_PATH):
+    return os.path.join(path, "My Drive")
 
 
 def is_drive_mounted(path=DEFAULT_PATH):
-    return os.path.isdir(path) and os.path.isdir(os.path.join(path, "Colab Notebooks"))
+    return os.path.isdir(get_drive_root(path)) and \
+           os.path.isdir(os.path.join(get_drive_root(path), "Colab Notebooks"))
 
 
 def mount_drive(path=DEFAULT_PATH):
